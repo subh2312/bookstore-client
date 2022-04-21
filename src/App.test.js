eeeb350debc2b296deca6d58db1d90d7a273dee3
Subhankar <Subhankar@zkteco.in>
2022-04-21 18:16:43 +0530

    Configured travis and heroku

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {render} from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App component', ()=>{
+    it('should render app with error', ()=>{
+        const {asFragment}=render(<App/>);
+        expect(asFragment()).toMatchInlineSnapshot(`
+<DocumentFragment>
+  <div>
+    My First Component
+  </div>
+</DocumentFragment>
+`);
+    })
+})
